b55c74c5693184d3595ac963296add7f7215600d
bryankiriama <bryankiriama@gmail.com>
2025-11-25 11:51:52 +0300

    created a table bears

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (names, age, sex, color, temperament, alive) VALUES
+("Mr. Chocolate",23, "M", "black", "kind",1),
+("Rowdy",25, "M", "black", "rude",1),
+("Tabitha",19, "F", "brown", "assmatic",1),
+("Sergeant Brown",14, "M", "brown", "agressive",0),
+("Melissa",20, "F", "brown", "lovely",1),
+("Tabitha",19, "F", "brown", "assmatic",1),
+("Tabitha",19, "F", "brown", "assmatic",1),
